da9178c81bc3dc96bfc5e922159a8b73d1aa67d7
Ekep Obasi <ekepobasi@gmail.com>
2023-09-10 03:03:16 +0100

    refactor: improved on mobile reponsiveness

--- src/Pages/Hooks/useIncrement.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-function useIncrement(defVal = null) {
-  const [state, setState] = useState(defVal);
-  return [state, setState];
-}
-
-export default useIncrement;
--- src/Pages/api.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-console */
-const URL =
-  'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean';
-
-const getQuestions = () => fetch(URL).then((res) => res.json());
-export default getQuestions;
