99db66edcfa1de1aaa5aa091a9cb65ce87b43b5d
Ekep Obasi <ekepobasi@gmail.com>
2023-09-10 03:02:04 +0100

    refactor: moved quiz and score state to the QuizContext Provider and exported it as a component to wrap the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import './Pages/App.css';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import QuizTemplate from './Views/Quiz';
-import Results from './Pages/Results';
-import Guide from './Views/Guide';
-import { PageProvider } from './Controller/Context';
-import useFetch from './Pages/Hooks/useFetch';
-import useIncrement from './Pages/Hooks/useIncrement';
+import QuizTemplate from './pages/Quiz';
+import Results from './pages/Results';
+import Guide from './pages/Guide';
+import QuizProvider from './context/QuizContext';
 
 function App() {
-  const [quiz] = useFetch();
-  const [score, setScore] = useIncrement(0);
-
   return (
-    <PageProvider value={{ quiz, score, setScore }}>
+    <QuizProvider>
       <BrowserRouter>
         <Routes>
           <Route index element={<Guide />} />
@@ -21,7 +16,7 @@ function App() {
           <Route path="/results" element={<Results />} />
         </Routes>
       </BrowserRouter>
-    </PageProvider>
+    </QuizProvider>
   );
 }
 
