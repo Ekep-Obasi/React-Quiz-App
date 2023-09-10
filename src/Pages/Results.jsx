dceec61f19f7f6c06266d4dbdc8a849b033b9f1a
Ekep Obasi <ekepobasi@gmail.com>
2023-09-10 03:03:16 +0100

    refactor: improved on mobile reponsiveness

--- src/Pages/Results.jsx
+++ src/Pages/Results.jsx
@@ -1,28 +1,37 @@
 /* eslint-disable react/no-danger */
 /* eslint-disable camelcase */
 import styled from '@emotion/styled';
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { PageConsumer } from '../Controller/Context';
+import { useQuiz } from '../context/QuizContext';
 
 const StyledContent = styled.div`
   display: flex;
+  min-height: 100vh;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: #343f4f;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
+  padding: 1rem;
 
   h1 {
-    font-size: 60px;
+    font-size: 3.75rem;
     color: #fff;
     font-weight: 500;
-    margin: 2px;
+    margin: 8px 0;
+
+    @media screen and (max-width: 72em) {
+      font-size: 2.75rem;
+    }
   }
 
   .score {
-    font-size: 36px;
+    font-size: 2.25rem;
     color: green;
+
+    @media screen and (max-width: 72em) {
+      font-size: 1.75rem;
+    }
   }
 
   button {
@@ -40,12 +49,17 @@ const StyledContent = styled.div`
 
 const StyledReviews = styled.div`
   color: white;
-  font-size: 18px;
+  font-size: 1.25rem;
   width: 80%;
   margin: auto;
   display: flex;
   align-items: center;
   gap: 5px;
+
+  @media screen and (max-width: 72em) {
+    width: 100%;
+    font-size: 1rem;
+  }
 
   .answer {
     color: yellow;
@@ -56,46 +70,49 @@ const StyledReviews = styled.div`
   }
 `;
 
-function Results() {
-  const { setScore } = useContext(PageConsumer);
+function ResultList() {
+  const { quiz } = useQuiz();
+
   return (
-    <PageConsumer>
-      {({ quiz, score }) => {
+    <>
+      {quiz.map(({ question, correct_answer }, index) => {
         return (
-          <StyledContent>
-            <h1>Results Page</h1>
-            <div className="score">You Score {score} / 100</div>
-            <div>
-              {quiz.map(({ question, correct_answer }, index) => {
-                return (
-                  <StyledReviews>
-                    <span
-                      key={question}
-                      className="numbers"
-                      dangerouslySetInnerHTML={{ __html: index + 1 }}
-                    />
-                    .
-                    <span
-                      className="questions"
-                      dangerouslySetInnerHTML={{ __html: question }}
-                    />
-                    <span
-                      className="answer"
-                      dangerouslySetInnerHTML={{ __html: correct_answer }}
-                    />
-                  </StyledReviews>
-                );
-              })}
-            </div>
-            <Link to="/">
-              <button type="button" onClick={() => setScore(0)}>
-                Back to Home
-              </button>
-            </Link>
-          </StyledContent>
+          <StyledReviews>
+            <span
+              key={question}
+              className="numbers"
+              dangerouslySetInnerHTML={{ __html: index + 1 }}
+            />
+            .
+            <span
+              className="questions"
+              dangerouslySetInnerHTML={{ __html: question }}
+            />
+            <span
+              className="answer"
+              dangerouslySetInnerHTML={{ __html: correct_answer }}
+            />
+          </StyledReviews>
         );
-      }}
-    </PageConsumer>
+      })}
+    </>
+  );
+}
+
+function Results() {
+  const { setScore, score } = useQuiz();
+
+  return (
+    <StyledContent>
+      <h1>Results Page</h1>
+      <div className="score">Your Score {score} / 100</div>
+      <ResultList />
+      <Link to="/">
+        <button type="button" onClick={() => setScore(0)}>
+          Back to Home
+        </button>
+      </Link>
+    </StyledContent>
   );
 }
 
