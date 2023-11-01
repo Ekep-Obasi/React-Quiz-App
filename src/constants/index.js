ebb199695fa0e7d4bda16261f041c0ab8bacdc7d
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-11-01 22:44:40 +0100

    exported question difficultylevels and question categories

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
 export const API_URL =
   process.env.REACT_APP_API_URL ||
   'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean';
-export default {};
+
+export { quizCategories } from './questions';
+export { difficultyLevel } from './questions';
---
+++ src/constants/questions.js
@@ -0,0 +1,31 @@
+export const difficultyLevel = ['Any', 'Difficulty', 'Easy', 'Medium', 'Hard'];
+
+// Quiz Categories
+export const quizCategories = [
+  'General Knowledge',
+  'Entertainment: Books',
+  'Entertainment: Film',
+  'Entertainment: Music',
+  'Entertainment: Musical & Theaters',
+  'Entertainment: Television',
+  'Entertainment: Video Games',
+  'Entertainment: Board Games ',
+  'Science & Nature',
+  'Science: Computers',
+  'Science: Mathematics',
+  'Mythology',
+  'Sports',
+  'Geography',
+  'History',
+  'Politics',
+  'Art',
+  'Celebrities',
+  'Animals',
+  'Vehicles',
+  'Entertainment',
+  'Science',
+  'Entertainment: Comics',
+  'Entertainment: Gadgets',
+  'Entertainment: Japanese Anime & Manga',
+  'Entertainment: Cartoon & Animations',
+];
