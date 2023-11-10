2c1f6a70e37ac2275f110317eaeeceeae9c95e84
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-11-10 12:24:06 +0100

    chore: cleaned fetching logic

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,12 +1,10 @@
-import GenerateSessionToken from './generateSessionToken';
+// import GenerateSessionToken from './generateSessionToken';
 import { httpClient } from './httpClient';
 
-export default async function fetchQuizes(query="") {
-  const sessiontoken = await GenerateSessionToken();
-  
+export default async function fetchQuizes(query= "amount=10") {
   try {
-    const response = await httpClient.get(`/api.php/?${query}&token=${sessiontoken}`);
-    return response.data.results;
+    const response = await httpClient.get(`/api.php/?${query}`);
+    return {response: response.data.results, errorCode: response.data.response_code};
   } catch (err) {
     throw new Error('Failed To fetch data');
   }
